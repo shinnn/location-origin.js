0d896bca8d512d579c8bd0f8288e12402ee3eeaa
shinnn <snnskwtnb@gmail.com>
2014-04-28 10:54:52 +0900

    improve test file

--- test.js
+++ test.js
@@ -1,22 +1,23 @@
 require('./dist/location-origin.js');
 var test = require('tape');
 
-test(
-  'window.location.origin should returns protocol, hostname and port number.',
-  function(t) {
-    'use strict';
+test('window.location.origin', function(t) {
+  'use strict';
     
-    t.plan(1);
+  t.plan(1);
   
-    var port = '';
-    if (window.location.port) {
-      port = ':' + window.location.port;
-    }
-  
-    var expected = window.location.protocol + '//' +
-                   window.location.hostname +
-                   port;
-  
-    t.equal(location.origin, expected);
+  var port = '';
+  if (window.location.port) {
+    port = ':' + window.location.port;
   }
-);
+  
+  var expected = window.location.protocol + '//' +
+                 window.location.hostname +
+                 port;
+  
+  t.equal(
+    location.origin,
+    expected,
+    'should returns protocol, hostname and port number.'
+  );
+});
