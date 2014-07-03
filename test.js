d863b35009ea5d1d706ebe52bb3befb75d8b6c9a
shinnn <snnskwtnb@gmail.com>
2014-07-03 21:45:35 +0900

    add and run .editorconfig

--- test.js
+++ test.js
@@ -3,18 +3,18 @@ var test = require('tape');
 
 test('window.location.origin', function(t) {
   'use strict';
-    
+
   t.plan(1);
-  
+
   var port = '';
   if (window.location.port) {
     port = ':' + window.location.port;
   }
-  
+
   var expected = window.location.protocol + '//' +
                  window.location.hostname +
                  port;
-  
+
   t.equal(
     location.origin,
     expected,
